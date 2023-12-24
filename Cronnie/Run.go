5d5ea4931f744b08a82b619f3dddeecbedde2610
Xantios Krugor <git@xantios.nl>
2023-12-24 16:52:07 +0100

    Moved garbage collector to GarbageCollector.go

--- Cronnie/Run.go
+++ Cronnie/Run.go
@@ -81,51 +81,6 @@ func (ci *Instance) Run() error {
 	}
 }
 
-func (ci *Instance) garbageCollector() {
-	for {
-
-		expired := time.Now().Add(-ci.keepCompleted)
-		q := `SELECT id FROM jobs where completed_at < $1`
-
-		r, e := ci.conn.Query(ci.ctx, q, expired.UTC())
-		if e != nil {
-			ci.logger.Printf("Error while running garbage collection. Error :: %s\n", e)
-			continue
-		}
-
-		var oldItems []int
-		for r.Next() {
-			var id int
-			e := r.Scan(&id)
-			if e != nil {
-				fmt.Printf("Cant unmarshal :: %#v\n", e)
-			}
-			oldItems = append(oldItems, id)
-		}
-
-		if len(oldItems) == 0 {
-			time.Sleep(time.Second * 30)
-			continue
-		}
-
-		// convert old items to csv
-		stringValues := make([]string, len(oldItems))
-		for i, v := range oldItems {
-			stringValues[i] = fmt.Sprint(v)
-		}
-		csv := strings.Join(stringValues, ",")
-
-		q = `DELETE FROM jobs WHERE id IN(` + csv + `);`
-		_, e = ci.conn.Query(ci.ctx, q)
-		if e != nil {
-			ci.logger.Printf("Error while dropping items %s", e)
-			continue
-		}
-
-		time.Sleep(time.Second * 30)
-	}
-}
-
 func (ci *Instance) crashRecovery() error {
 	jobs, err := ci.GetJobs()
 	if err != nil {
